23af5d09d15390ccdaa77c25722fb65e2864ebf4
ahmedWessamTest <ahmedwessam202@gmail.com>
2026-06-15 14:48:59 +0200

    feat: implement HeroSection component and update index.html structure

--- src/app/featurs/home/components/hero-seciton/hero-seciton.component.ts
+++ src/app/featurs/home/components/hero-seciton/hero-seciton.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
+import { NgOptimizedImage } from '@angular/common';
 import { MarkataImgPlaceholderDirective } from '../../../../shared/directives/markata-img-placeholder.directive';
 import { LanguageService } from '../../../../core/services/language.service';
 
 @Component({
   selector: 'app-hero-seciton',
-  imports: [TranslatePipe, MarkataImgPlaceholderDirective, RouterLink],
+  imports: [TranslatePipe, MarkataImgPlaceholderDirective, RouterLink, NgOptimizedImage],
   templateUrl: './hero-seciton.component.html',
   styleUrl: './hero-seciton.component.css',
 })
