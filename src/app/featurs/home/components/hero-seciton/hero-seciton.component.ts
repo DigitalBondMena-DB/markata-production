2617a64694145b08724a2ee686730df0f29b8616
ahmedWessamTest <ahmedwessam202@gmail.com>
2026-06-15 14:57:02 +0200

    feat: implement home page hero section and global base styles

--- src/app/featurs/home/components/hero-seciton/hero-seciton.component.ts
+++ src/app/featurs/home/components/hero-seciton/hero-seciton.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
-import { NgOptimizedImage } from '@angular/common';
 import { MarkataImgPlaceholderDirective } from '../../../../shared/directives/markata-img-placeholder.directive';
 import { LanguageService } from '../../../../core/services/language.service';
 
 @Component({
   selector: 'app-hero-seciton',
-  imports: [TranslatePipe, MarkataImgPlaceholderDirective, RouterLink, NgOptimizedImage],
+  imports: [TranslatePipe, MarkataImgPlaceholderDirective, RouterLink],
   templateUrl: './hero-seciton.component.html',
   styleUrl: './hero-seciton.component.css',
 })
